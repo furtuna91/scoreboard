238fed5849de4ff9472b6e2e3b4ac469317e9c6c
Furtuna Virgil <virgil.furtuna@gmail.com>
2019-05-21 19:05:36 +0300

    Add a new player functionality

--- src/components/App.js
+++ src/components/App.js
@@ -30,10 +30,26 @@ class App extends React.Component {
 		]
 	};
 
+	// player id counter
+	prevPlayerId = 4;
+
 	handleScoreChange = (index, delta) => {
 		this.setState( prevState => ({
 			score: prevState.players[index].score += delta
 		}));
+	}
+
+	handleAddPlayer = (name) => {
+		this.setState( {
+			players: [
+				...this.state.players,
+				{
+					name,
+					score: 0,
+					id: this.prevPlayerId += 1
+				}
+			]
+		} )
 	}
 
 	handleRemovePlayer = (id) => {
@@ -65,7 +81,7 @@ class App extends React.Component {
 						removePlayer={this.handleRemovePlayer}           
 					/>
 					)}
-				<AddPlayerForm />
+				<AddPlayerForm addPlayer={this.handleAddPlayer} />
 			</div>
 		);
 	}
